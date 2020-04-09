c030647b3e0780f39734255e409b47da903e804f
jerrywithaz <zerry.social@gmail.com>
2020-04-09 15:33:16 -0500

    renamed redux folder

--- src/providers/AppProvider/AppProvider.component.tsx
+++ src/providers/AppProvider/AppProvider.component.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { Provider } from 'react-redux';
-import configureStore from 'app/configureStore';
+import configureStore from 'app-redux/configureStore';
 
 const store = configureStore({});
 
